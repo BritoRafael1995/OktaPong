cd5324fa1a62b5ec8a729f0213f3e768ee7b5557
Rafael Brito <rbrito>
2020-07-16 18:59:27 -0300

    criado script projetil

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity.Set(2f, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
